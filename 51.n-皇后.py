98d7d987f4fe755c144ad2b0d00f14e9c5cf4466
chyao7 <chayo7@163.com>
2022-01-06 21:20:19 +0800

    n-皇后

---
+++ 51.n-皇后.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode.cn id=51 lang=python3
+#
+# [51] N 皇后
+#
+
+# @lc code=start
+class Solution:
+    def solveNQueens(self, n: int):
+        self.a = [0]*n
+        res = []
+        res.append(self.dfs(n,0))
+        print(res)
+        return 
+
+    def dfs(self,n,row):
+        if row == n:
+            print(self.a)
+            return self.a
+        for i in range(n):
+            if self.check(row, i):
+                self.a[row] = i
+                print (self.a, i, self.a[i])
+                self.dfs(n,row+1)
+                self.a[row] = 0
+
+    def check(self,row ,e):
+        for i in range(row):
+            if self.a[i]==e: return False
+            if (self.a[i]+i)==(row+e):return False
+            if (self.a[i]-i) == (e-row): return False
+        return True
+
+
+
+
+        
+
+# @lc code=end
+
+if __name__=="__main__":
+    s = Solution()
+    s.solveNQueens(n=4)
